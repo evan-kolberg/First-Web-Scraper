354cd2e0d988e5ec9febf272ef41cc4de6e427cf
Evan <ekprogamer13@gmail.com>
2022-03-09 18:05:45 -0500

    scrapes all badges in kidOYO

---
+++ oyoclass scraper.py
@@ -0,0 +1,29 @@
+import requests
+from bs4 import BeautifulSoup
+
+postLoginUrl = 'https://oyoclass.com/signin'
+
+pageNum = 1
+scrapingUrl = f'https://bmsteam.oyoclass.com/badges/all?page='
+
+payload = {
+    'username': input('Email:  '),
+    'password': input('Password:  ')
+}
+
+# sessions are used to log into sites
+with requests.Session() as session:
+    # this sends the payload to log in
+    post = session.post(postLoginUrl, data=payload)
+
+    # <class 'bs4.element.ResultSet'> ~ This is a custom RuleSet
+    while pageNum <= 24:
+        page = session.get(scrapingUrl + str(pageNum))
+        soup = BeautifulSoup(page.content, 'html.parser')
+
+        for i in soup.findAll('p', {'class': 'cat_badge_name'}):
+            print(i.get_text())  # the .get_text() removes the tags
+
+        pageNum += 1
+
+
